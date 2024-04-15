60faf486c05907ca856eb82d66e5323a4ae4a1de
Elysio Neto <e_damas@hotmail.com>
2024-04-15 20:08:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# app.py
 import streamlit as st
 
-st.title('Campus Virtual')
+# Your Streamlit app code here
+def main():
+  st.title('Campus Virtual')
+
+if __name__ == '__main__':
+  st.set_option('server.enableCORS', True) 
+  main()
